1046fab1ef17c9b1232ffaef8f931689a76c5edf
mycodesgit <lowexian@gmail.com>
2026-02-23 13:17:34 +0800

    Update

--- resources/views/reports/prntsummaryevalresult/evaluationsummaryprint.blade.php
+++ resources/views/reports/prntsummaryevalresult/evaluationsummaryprint.blade.php
@@ -39,6 +39,7 @@
                                             <div class="col-md-3 mb-3">
                                                 <label>School Year<span class="text-danger">*</span></label>
                                                 <select class="form-control form-control-sm" name="schlyear" id="schlyear">
+                                                    <option disabled selected> --Select-- </option>
                                                     @foreach($currsem as $datacurrsem)
                                                         <option 
                                                             value="{{ $datacurrsem->qceschlyear }}"
@@ -57,9 +58,9 @@
                                                 <label>Semester<span class="text-danger">*</span></label>
                                                 <select class="form-control form-control-sm" name="semester" id="semester">
                                                     <option disabled selected>Select</option>
-                                                    <option value="1" @if (old('type') == 1) {{ 'selected' }} @endif>First Semester</option>
-                                                    <option value="2" @if (old('type') == 2) {{ 'selected' }} @endif>Second Semester</option>
-                                                    <option value="3" @if (old('type') == 3) {{ 'selected' }} @endif>Summer</option>
+                                                    <option value="1" id="sem1">First Semester</option>
+                                                    <option value="2" id="sem2">Second Semester</option>
+                                                    <option value="3" id="sem3">Summer</option>
                                                 </select>
                                             </div>
 
@@ -106,5 +107,24 @@
 
         // Run once on page load for the default selected value
         document.getElementById('schlyear').dispatchEvent(new Event('change'));
+    </script>
+
+    <script>
+        const schoolYear = document.getElementById('schlyear');
+        const sem1 = document.getElementById('sem1');
+        const semester = document.getElementById('semester');
+
+        schoolYear.addEventListener('change', function () {
+
+            semester.selectedIndex = 0;
+
+            if (this.value === '2025-2026') {
+                sem1.disabled = true;
+                sem1.style.display = 'none';
+            } else {
+                sem1.disabled = false;
+                sem1.style.display = 'block';
+            }
+        });
     </script>
 @endsection
