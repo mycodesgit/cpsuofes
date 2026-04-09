6034e796263acf1592ee0d5b3c262f4adff37f5f
mycodesgit <lowexian@gmail.com>
2026-04-09 23:45:40 +0800

    Update

--- resources/views/users/listusr.blade.php
+++ resources/views/users/listusr.blade.php
@@ -42,17 +42,17 @@
     </div>
 
     <div class="modal fade" id="addUserModal" tabindex="-1" role="dialog" aria-labelledby="addUserModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-xl" role="document">
+        <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h6 class="modal-title" id="addUserModalLabel">Add New User</h6>
+                    <h6 class="modal-title" id="addUserModalLabel"><i class="ti ti-user-plus"></i> Add New User</h6>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <form class="form-horizontal p-3" action="{{ route('user.create') }}" method="post" id="addUser">  
                     @csrf
                     <div class="modal-body">
-                        <div class="form-group mb-3">
-                            <div class="row">
+                        <div class="form-group">
+                            <div class="row g-3">
                                 <div class="col-md-4">
                                     <label>First Name: <span class="text-danger">*</span></label>
                                     <input type="text" name="fname" oninput="var words = this.value.split(' '); for(var i = 0; i < words.length; i++){ words[i] = words[i].substr(0,1).toUpperCase() + words[i].substr(1); } this.value = words.join(' ');" placeholder="Enter First Name" class="form-control">
@@ -70,8 +70,8 @@
                             </div>
                         </div>
 
-                        <div class="form-group mb-3">
-                            <div class="row">
+                        <div class="form-group mt-3">
+                            <div class="row g-3">
                                 <div class="col-md-4">
                                     <label>Ext.: <span class="text-danger">*</span></label>
                                     <select class="form-control" name="ext">
@@ -96,8 +96,8 @@
                             </div>
                         </div>
 
-                        <div class="form-group mb-3"> 
-                            <div class="row">
+                        <div class="form-group mt-3"> 
+                            <div class="row g-3">
                                 <div class="col-md-4">
                                     <label>Campus <span class="text-danger">*</span></label>
                                     <select class="form-control" name="campus" required="">
@@ -155,7 +155,7 @@
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-outline-info" data-bs-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-outline-success">Save changes</button>
+                        <button type="submit" class="btn btn-success text-light"><i class="ti ti-device-floppy"></i> Save changes</button>
                     </div>
                 </form>
             </div>
