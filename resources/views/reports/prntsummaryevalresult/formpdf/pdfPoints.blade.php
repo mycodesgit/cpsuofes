581971494af5de3ef13101e0990ccaee7d408708
mycodesgit <lowexian@gmail.com>
2026-08-10 16:03:07 +0800

    Update

--- resources/views/reports/prntsummaryevalresult/formpdf/pdfPoints.blade.php
+++ resources/views/reports/prntsummaryevalresult/formpdf/pdfPoints.blade.php
@@ -184,11 +184,25 @@
         <span style="display: inline-block; width: 210px; vertical-align: top;">Reviewed and Discussed with:</span>
         <div style="display: inline-block; margin-left: 20px; vertical-align: top; text-align: center; border-bottom: 1px solid black; width: 250px;">
             @if(request('campus') === 'MC')
-                <span style="font-weight: bold; text-transform: uppercase;">{{ $reviewer }} </span> <span style="font-weight: bold;">{{ $rankrate }}</span>
-            @elseif(request('campus') === 'IC' || request('faclty') === 1188)
-                <span style="font-weight: bold; text-transform: uppercase;">MARY GRACE NOREEN P. LEDUNA-JARANILLA, </span> <span style="font-weight: bold;"> Ph. D.</span>
+                <span style="font-weight: bold; text-transform: uppercase;">
+                    {{ $reviewer }}
+                </span>
+                <span style="font-weight: bold;">
+                    {{ $rankrate }}
+                </span>
+
+            @elseif(request('campus') === 'IC' && request('faclty') == 1188)
+                <span style="font-weight: bold; text-transform: uppercase;">
+                    MARY GRACE NOREEN P. LEDUNA-JARANILLA,
+                </span>
+                <span style="font-weight: bold;">
+                    Ph. D.
+                </span>
+
             @else
-                <span style="font-weight: bold; text-transform: uppercase;">{{ $reviewerCampAd }}</span>
+                <span style="font-weight: bold; text-transform: uppercase;">
+                    {{ $reviewerCampAd }}
+                </span>
             @endif
         </div>
     </div>
