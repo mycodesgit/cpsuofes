7c749260db733f07a0a028bc654ba7bcb4ed10a7
mycodesgit <lowexian@gmail.com>
2026-08-12 14:55:28 +0800

    Update

--- resources/views/reports/prntsummaryevalresult/formpdf/pdfSumSheet.blade.php
+++ resources/views/reports/prntsummaryevalresult/formpdf/pdfSumSheet.blade.php
@@ -124,7 +124,7 @@
         </table>
 	</div>    
 
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    {{-- <br><br><br><br><br><br><br><br><br><br><br><br><br><br> --}}
     <div class="details-sm" style="margin-top: 30px;">
         <span style="display: inline-block; width: 80px; vertical-align: top;">Recorded by:</span>
         <div style="display: inline-block; margin-left: 5px; vertical-align: top; text-align: center; border-bottom: 1px solid black; width: 205px; font-size: 9pt !important">
